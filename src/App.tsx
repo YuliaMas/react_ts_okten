47a8312c0238529b516206a591b0390ce5c7807b
Yuliya Masechko <yulia.masechko@gmail.com>
2024-07-04 00:23:43 +0300

    Add Decrement button

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,6 @@ const App: FC = () => {
     const array: string[] = ['Mila' , 'Mia', 'Sia', 'Lilia', 'Sofia'];
     const [count, setCount] = useState<number>(0)
     const [name, setName] = useState<string>(array[0]);
-    // const [name, setName] = useState<string>(array[0]);
     const [users, setUsers] = useState<IUser[]>([]);
     const[currentValue,previousValue] = usePrevious(count);
 
@@ -35,18 +34,26 @@ const App: FC = () => {
         <div className="App">
             <div>
                 <div>
-                     {previousValue} - {currentValue}
+                    {previousValue} - {currentValue}
                 </div>
                 <h2>{name}</h2>
                 <button onClick={() => setCount(currentCount => currentCount + 1)}>
                     Increment
+                </button>
+                <button onClick={() => setCount(currentCount => {
+                 return  currentCount< 1 ? 0 :( currentCount - 1)
+                })}>
+                    Decrement
                 </button>
                 {/*<button onClick={() => setName(array[currentValue]) }>Change Name</button>*/}
                 <button onClick={() => setName(users[currentValue].firstName)}>Change Name</button>
             </div>
 
             <h3>Click me, please...</h3>
-            <button onClick={() => {click()}}>Click</button>
+            <button onClick={() => {
+                click()
+            }}>Click
+            </button>
             <h4>{value.toString()}</h4>
             <Block styleClass={styleClass}>useState</Block>
         </div>
