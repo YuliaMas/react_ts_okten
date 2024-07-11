2a7f29dd50fb2ee40a9c22f3ed56f56c54e1c0ee
Yuliya Masechko <yulia.masechko@gmail.com>
2024-07-11 14:18:32 +0300

    Add  basic part HW6

---
+++ src/component/MenuComponent.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import {Link} from "react-router-dom";
+
+const MenuComponent = () => {
+    return (
+        <div>
+            <ul>
+                <li><Link to={''}>home</Link></li>
+                <li><Link to={'users'}>users page</Link></li>
+                <li><Link to={'posts'}>posts page</Link></li>
+                <li><Link to={'comments'}>comments page</Link></li>
+            </ul>
+        </div>
+    );
+}
+
+export default MenuComponent;
--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import MainLayout from "./layouts/MainLayout";
+import UsersPage from "./pages/UsersPage";
+import PostsPage from "./pages/PostsPage";
+import CommentsPage from "./pages/CommentsPage";
+import CustomErrorLayout from "./layouts/CustomErrorLayout";
+import HomePage from "./pages/HomePage";
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
+let router = createBrowserRouter([
+    {
+        path: '/',
+        element: <MainLayout/> ,
+        errorElement: <CustomErrorLayout/>,
+        children: [
+            {index: true, element: <HomePage/>},
+            {path: 'users', element: <UsersPage/>},
+            {path: 'posts', element: <PostsPage/>},
+            {path: 'comments', element: <CommentsPage/>},
+        ]}
+])
+
 root.render(
-    <App />
+    <RouterProvider router={router} />
 );
