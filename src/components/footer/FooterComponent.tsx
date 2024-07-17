d560663d46b31be165c8a610d3778e304d1bf30b
Yuliya Masechko <yulia.masechko@gmail.com>
2024-07-17 18:44:53 +0300

    Add footer to HW7

---
+++ src/components/footer/FooterComponent.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./FooterComponent.css";
+
+const FooterComponent = () => {
+    return (
+        <div className={"footer"}>
+            <p>Copyright by Yuliya Masechko , 2024</p>
+        </div>
+    );
+};
+
+export default FooterComponent;
--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import {Outlet} from "react-router-dom";
 import MenuComponent from "../components/menu/MenuComponent";
+import FooterComponent from "../components/footer/FooterComponent";
 
 const MainLayout = () => {
     return (
         <div>
             <MenuComponent/>
             <Outlet/>
+            <FooterComponent/>
         </div>
     );
 };
