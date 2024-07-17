8692dff7c79d77683bcb783b257d072412c09fa3
Yuliya Masechko <yulia.masechko@gmail.com>
2024-07-17 18:52:45 +0300

    Changed PostPage.tsx HW7

--- src/pages/PostPage.tsx
+++ src/pages/PostPage.tsx
@@ -1,23 +1,23 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {Link, useParams} from "react-router-dom";
-import {useAppLocation} from "../hook/useAppLocation";
 import {IPost} from "../models/IPost";
 import PostInfoComponent from "../components/PostInfoComponent";
+import {getPostById} from "../services/api.service";
 
 const PostPage = () => {
     let {id} = useParams();
-    let location = useAppLocation();
-    let state: IPost =  location.state;
-
-    console.log(location);
-    console.log(id);
-    console.log(state);
+        const [post, setPost] = useState<IPost>();
+        useEffect(() => {
+            if (id) {
+                getPostById(id.toString()).then(value => setPost(value));
+            }
+        }, [id]);
 
     return (
         <div className={"main"}>
             <h1>Post Page</h1>
-            <PostInfoComponent  key={state.id} state={state}/>
-            <Link to={"/posts/" + id+ "/comments"} className={"post-link"}>
+            <PostInfoComponent key={post?.id} post={post}/>
+            <Link to={"/posts/" + id + "/comments"} state={post} className={"post-link"}>
                 Comments...
             </Link>
         </div>
